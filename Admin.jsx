6fd21106f7faef84eb74918ad15bc6749b12d689
Nehal Gaherwar <nehalgaherwar23@gmail.com>
2026-02-19 15:08:44 +0530

    Create Admin.jsx

---
+++ Admin.jsx
@@ -0,0 +1,10 @@
+import Charts from "../components/Charts";
+
+export default function Admin() {
+  return (
+    <div className="container">
+      <h1>Pollution Dashboard</h1>
+      <Charts />
+    </div>
+  );
+}
