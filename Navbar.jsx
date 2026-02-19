64a9aeec7178e699e60374be38eb134502c1a827
Nehal Gaherwar <nehalgaherwar23@gmail.com>
2026-02-19 15:04:06 +0530

    Create Navbar.jsx

---
+++ Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <h2>HydroGuard X</h2>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/admin">Dashboard</Link>
+      </div>
+    </nav>
+  );
+}
